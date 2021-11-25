5f6fdc26f6413c837c8b97f118295eff9e0517f7
Lena LE BILLAN <84079692+Lena-hub@users.noreply.github.com>
2021-11-25 22:42:27 +0100

    CSS propre

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,7 +3,6 @@ let bouton=document.querySelector("#jouer")
 let description=document.querySelector("#description")
 let artistes=document.querySelector(".artistes")
 let questions=document.querySelector(".questions")
-console.log(questions)
 
 bouton.addEventListener("click", e=>{
    bouton.style.display="none"
